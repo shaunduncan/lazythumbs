971876befd342a20a7ec0bb7521a9f12a711b310
Nate Smith <nate.smith@coxinc.com>
2012-02-09 10:52:41 -0500

    stub tests

--- lazythumbs/tests.py
+++ lazythumbs/tests.py
@@ -1,23 +1,19 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
+from unittest import TestCase
 
-Replace these with more appropriate tests for your application.
-"""
+class ColdCacheThumbnailTest(TestCase):
+    def setUp:
+        pass
+    def test_img_not_found(self):
+        pass
+    def test_img_found(self):
+        pass
 
-from django.test import TestCase
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
-
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
-"""}
-
+class WarmCacheThumbnailTest(TestCase):
+    def setUp:
+        pass
+    def test_was_404(TestCase):
+        pass
+    def test_hot_cache_cold_fs(TestCase):
+        pass
+    def test_hot_cache_hot_fs(TestCase):
+        pass
