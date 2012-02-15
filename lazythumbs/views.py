791e87ab9ab83433e8bcaa2b1b7012e6ed318f5f
Nate Smith <nate.smith@coxinc.com>
2012-02-15 15:16:47 -0500

    rewrite views. this version runs but is otherwise untested.

--- lazythumbs/views.py
+++ lazythumbs/views.py
@@ -1,148 +1,209 @@
+from hashlib import md5
 import json
 import os
+import re
+import types
 
 from django.conf import settings
 from django.core.cache import cache
+from django.core.files.storage import FileSystemStorage
+from django.core.files.base import ContentFile
+from django.core.exceptions import SuspiciousOperation
 from django.http import HttpResponse
+from django.views.generic.base import View
+from PIL import ImageOps, Image
 
-from sorl.thumbnail import default
-from sorl.thumbnail.base import ThumbnailBackend
-from sorl.thumbnail.images import ImageFile
-from sorl.thumbnail.parsers import parse_geometry
-
-# TODO
-# bug: generate_thumbnail shouldn't always write to the l
-
-def thumbnail(request, width, height, img_path):
+class LazyThumbRenderer(View):
     """
-    Generate a thumbnail from img_path using the supplied width and height.
-    a request for upscaling will generate a "thumbnail" but just leave the
-    image at its existing dimensions. Responds with an image/jpeg. Defaults to
-    a "resize and crop" with a left crop by default.
-
-    :param request: HttpRequest object
-    :param img_path: image filesystem path relative to THUMBNAIL_SOURCE
-    :param width: integer width in pixels
-    :param height: integer height in pixels
+    Perform requested image render operations and handle fs logic and caching
+    of 404s. Maps a requested action (currently 'thumbnail' and 'resize' are
+    supported) to a matching method named after the action prefixed with
+    'action_'. Once the argument signatures are relaxed one can implement new
+    image transformations simply by subclassing this view and adding "action_"
+    methods that return raw image data as a string.
     """
-    def four_oh_four():
+    fs = FileSystemStorage()
+    def action(fun):
+        """
+        Decorator used to denote an instance method as an action: a function
+        that takes a path to an image, performs PIL on it, and returns raw imag
+        data.
+        """
+        fun.is_action = True
+        return fun
+
+    @property
+    def _allowed_actions(self):
+        """
+        Builds internal list of allowed actions (methods denoted with
+        @action)
+        """
+        return [
+            a for a in dir(self)
+            if (lambda x: type(x) == types.MethodType
+                and getattr(x, 'is_action', False))(getattr(self, a))
+        ]
+
+    def get(self, request, action, geometry, source_path):
+        """
+        Perform action routing and handle sanitizing url input. Handles caching the path to a rendered image to
+        django.cache and saves the new image on the filesystem. 404s are cached to
+        save time the next time the missing image is requested.
+
+        :param request: HttpRequest
+        :param action: some action, eg thumbnail or resize
+        :param geometry: a string of either '\dx\d' or just '\d'
+        :param source_path: the fs path to the image to be manipulated
+        :returns: an HttpResponse with an image/jpeg content_type
+        """
+        source_path = os.path.join(settings.THUMBNAIL_SOURCE_PATH, source_path)
+
+        # reject naughty paths and actions
+        if source_path.startswith('/'):
+            return self.four_oh_four()
+        if re.match('\.\./', source_path):
+            return self.four_oh_four()
+        if action not in self.allowed_actions:
+            return self.four_oh_four()
+
+        try:
+            w,h = geometry.split('x')
+        except ValueError:
+            w = geometry
+            h = None
+
+        cache_key = self.cache_key(source_path, action, width, height)
+        source_meta = cache.get(cache_key)
+
+        if source_meta:
+            source_meta = json.loads(img_meta)
+            was_404 = source_meta['was_404']
+            rendered_path = source_meta['rendered_path']
+            rendered_path = os.path.join(settings.LAZYTHUMB_SOURCE_PATH, rendered_path)
+
+            try:
+                f = self.fs.open(rendered_path)
+                raw_data = f.read()
+            except IOError:
+                _, raw_data = self.render_and_save(source_path, width, height)
+            except SuspiciousOperation:
+                return self.four_oh_four()
+            finally:
+                return self.two_hundred(raw_data)
+
+        if self.fs.exists(source_path):
+            rendered_path, raw_data = self.render_and_save(source_path, width, height)
+            response = self.two_hundred(raw_data)
+            source_meta = dict(rendered_path=rendered_path, was_404=False)
+            expires = settings.LAZYTHUMB_CACHE_TIMEOUT
+        else:
+            response = self.four_hundred()
+            source_meta = dict(rendered_path='', was_404=True)
+            expires = settings.LAZYTHUMB_404_CACHE_TIMEOUT
+
+        cache.set(cache_key, json.dumps(source_meta), expires)
+
+        return response
+
+    def render_and_save(self, img_path, width, height):
+        """
+        Defers to action_ methods to actually manipulate an image. Saves the
+        resulting image to the filesystem.
+
+        :returns rendered_path: fs path to new image
+        :returns raw_data: raw data of new image as string
+        """
+        action_hash = self.hash_(img_path, action, width, height)
+        rendered_path = '%s/%s/%s' % (action_hash[0:2], action_hash[2:4], action_hash)
+        raw_data = getattr(self, action)(img_path, width, height)
+        self.fs.save(rendered_path, ContentFile(raw_data))
+
+        return rendered_path, raw_data
+
+    def cache_key(self, img_path, action, width, height):
+        """
+        Compute a unique cache key for an image operation. Takes width, height,
+        fs path, and desired action into account.
+
+        :param img_path: fs path to a source image
+        :param action: string representing image manipulation to occur
+        :param width: integer width in pixels
+        :param height: integer height in pixels
+        """
+        hashed = self.hash_(img_path, action, width, height)
+        return 'lazythumbs:%s' % hashed
+
+    def hash_(self, img_path, action, width, height):
+        """
+        Generate an md5 hash for an image operation. Takes width, height,
+        fs path, and desired action into account.
+
+        :param img_path: fs path to a source image
+        :param action: string representing image manipulation to occur
+        :param width: integer width in pixels
+        :param height: integer height in pixels
+        """
+        hashed = md5('%s:%s:%s:%s' % img_path, action, width, height)
+        return hashed.hexdigest()
+
+    def two_hundered(img_data):
+        """
+        Generate a 200 image response with raw image data, Cache-Control set,
+        and an image/jpeg content-type.
+
+        :param img_data: raw image data as a string
+        """
+        resp = HttpResponse(img_data, content_type='image/jpeg')
+        resp['Cache-Control'] = 'public,max-age=%s' % settings.THUMBNAIL_CACHE_TIMEOUT
+        return resp
+
+    def four_oh_four(self):
+        """
+        Generate a 404 response with an image/jpeg content_type. Sets a
+        Cahce-Control header for caches like Akamai (browsers will ignore it
+        since it's a 4xx.
+        """
         resp = HttpResponse(status=404, content_type='image/jpeg')
         resp['Cache-Control'] = 'public,max-age=%s' % settings.THUMBNAIL_404_CACHE_TIMEOUT
         return resp
 
-    def two_hundred(data):
-        resp = HttpResponse(data, content_type='image/jpeg')
-        resp['Cache-Control'] = 'public,max-age=%s' % settings.THUMBNAIL_CACHE_TIMEOUT
-        return resp
+    @action
+    def thumbnail(self, img_path, width, height=None):
+        """
+        Scale in one or two dimensions and then perform a left crop (if only
+        one dimension was specified).
 
-    geometry = '%sx%s' % (width, height)
-    img_path = os.path.join(settings.THUMBNAIL_SOURCE_PATH, img_path)
-    cache_key = img_cache_key(img_path, width, height, 'thumbnail')
+        :param img_path: path to an image to be thumbnailed
+        :param width: width in pixels of desired thumbnail
+        :param height: height in pixels of desired thumbnail (optional)
 
-    img_meta = cache.get(cache_key)
-    if img_meta: #, then we've processed this already. see if it exists.
-        img_meta = json.loads(img_meta)
-        was_404 = img_meta['was_404']
-        thumbnail_path = img_meta['thumbnail_path']
-        thumbnail_path = os.path.join(settings.THUMBNAIL_SOURCE_PATH, thumbnail_path)
+        :returns: raw image data as a string
+        """
+        img = Image.open(img)
+        do_crop = False
+        if height is None:
+            do_crop = True
+            height = int(img.size[1] * (float(width) / img.size[0]))
+        width = min(width, img.size[1])
+        height = min(height, img.size[0])
 
-        if was_404: #, then nothing to do. 404.
-            return four_oh_four()
+        img = img.resize((width, height), Image.ANTIALIAS)
 
-        # check and see if img is in cache but not filesystem.
-        try:
-            thumbnail = ImageFile(thumbnail_path, default.storage)
-            thumbnail_raw = thumbnail.read()
-        except IOError:
-            _, thumbnail_raw = generate_thumbnail_from_path(img_path, width, height)
-        finally:
-            return two_hundred(thumbnail_raw)
+        if do_crop:
+            img = img.crop((0,0, width, width))
 
-    # img was not found in cache. we need to look for and process the image.
-    if (os.path.exists(img_path)): #, then we can process it.
-        thumbnail_path, thumbnail_raw = generate_thumbnail_from_path(img_path, width, height)
+        return img.tostring()
 
-        img_meta = {'thumbnail_path':thumbnail_path, 'was_404':False}
-        cache.set(cache_key, json.dumps(img_meta), settings.THUMBNAIL_CACHE_TIMEOUT)
+    @action
+    def resize(self, img_path, width, height):
+        """
+        resize to given dimenions.
 
-        return two_hundred(thumbnail_raw)
+        :param img_path: path to an image to be resizeed
+        :param width: width in pixels of new image
+        :param height: height in pixels of  new image
 
-    # finally, give up and 404. Cache the 404 so we don't do this dance again.
-    img_meta_json = json.dumps({'thumbnail_path':'', 'was_404':True})
-    expires = settings.THUMBNAIL_404_CACHE_TIMEOUT
-    cache.set(cache_key, img_meta_json, expires)
-
-    return four_oh_four()
-
-def default_thumbnail(request, img_path):
-    """
-    simple wrapper around thumbnail that passes
-    settings.THUMBNAIL_DEFAULT_WIDTH and settings.THUMBNAIL_DEFAULT_HEIGHT
-
-    :param img_path: image filesystem path relative to THUMBNAIL_SOURCE
-    """
-    width = settings.THUMBNAIL_DEFAULT_WIDTH
-    height = settings.THUMBNAIL_DEFAULT_HEIGHT
-    return thumbnail(request, width, height, img_path)
-
-def img_cache_key(img_path, width, height, action):
-    """
-    returns a cache key suitable for storing thumbnail metadata. We need a key
-    that represents both the action taken and the target geometry of the newly
-    created image.
-
-    :param img_path: path to image relative to settings.THUMBNAIL_SOURCE
-    :param width: target width in pixels for new image
-    :param height: target height in pixels for new image
-    :param action: string representing the action taken, eg "thumbnail"
-    :returns: a cache key
-    """
-    return "lazythumbs:%s:%sx%s:%s" % (action, width, height, img_path)
-
-def generate_thumbnail_from_path(img_path, width, height, crop='left'):
-    """
-    create and save to the filesystem a thumbnail based on the provided path to
-    a source image.
-
-    side effect: writes to the filesystem.
-
-    :param img_path: path to an image stored on the filesystem
-    :param width: integer width in pixels
-    :param height: integer height in pixels
-    :param crop: one of (top|left|bottom|right|center). Defaults to 'left'.
-    :returns: a tuple (path_to_thumbnail, raw_image_data)
-    """
-    geometry = '%sx%s' % (width, height)
-    tb = ThumbnailBackend()
-    options = tb.default_options
-    options['crop'] = crop
-
-    # source is an ImageFile; a sorl-thumbnail abstraction for image
-    # files on the fs. source_img is an Image; a wrapper around
-    # in-memory PIL (or whatever engine) image data.
-    source = ImageFile(img_path, default.storage)
-    source_img = default.engine.get_image(source)
-    source.set_size(default.engine.get_image_size(source_img))
-
-    thumbnail_path = tb._get_thumbnail_filename(source, geometry, options)
-    thumbnail = ImageFile(thumbnail_path, default.storage)
-
-    # create the thumbnail in memory
-    ratio = default.engine.get_image_ratio(source)
-    geometry = parse_geometry(geometry, ratio)
-    thumbnail_img = default.engine.create(source_img, geometry, options)
-
-    # extract its raw data
-    f = options['format']
-    q = options['quality']
-    prog = options.get('progressive', settings.THUMBNAIL_PROGRESSIVE)
-    thumbnail_raw = default.engine._get_raw_data(thumbnail_img, f, q, progressive=prog)
-
-    # save raw data to filesystem
-    # we don't have to worry about filesystem collisions (and
-    # subsequent file corruption) since django's storage abstraction
-    # layer handles all that for us (thanks django).
-    thumbnail.write(thumbnail_raw)
-
-    return thumbnail_path, thumbnail_raw
-
+        :returns: raw image data as a string
+        """
+        img = Image.open(img)
+        return img.resize((width, height), Image.ANTIALIAS).tostring()
