8e86fd407e167da38c23dfd74425f01b24a01f15
Paul <paul.mcgoldrick@coxinc.com>
2012-10-09 10:34:36 -0700

    [CMSIMPG-366] Backwards compatible urls

--- lazythumbs/urls.py
+++ lazythumbs/urls.py
@@ -4,4 +4,5 @@ from lazythumbs.views import LazyThumbRenderer
 urlpatterns = patterns('',
     # we'll cleanse the liberal .+ in the view.
     (r'lt_cache/(\w+)/(\d+\/\d+|\d+)/(.+)$', LazyThumbRenderer.as_view()),
+    (r'lt_cache/(\w+)/(\d+x\d+|\d+)/(.+)$', LazyThumbRenderer.as_view()),
 )
