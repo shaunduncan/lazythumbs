ef2892bac79857b8fb39a66ea85043bc7b36ce22
Aaron McCall <aaron@andyet.net>
2012-10-19 14:27:37 -0700

    [JANUS-457] create helper to retrieve source image attrs

--- lazythumbs/util.py
+++ lazythumbs/util.py
@@ -200,10 +200,18 @@ def get_format(file_path):
             return "JPEG"  # if we don't recognize the format, assume JPEG
     return fmt
 
+
 def get_attr_string(img):
     """ given an image attr dict like that returned by compute_img or get_img_attrs get the string of height width attrs for an img tag """
     attrs = ['%s="%s"' % attr for attr in img.items() if attr[1]]
     return " ".join(attrs)
+
+
+def get_source_img_attrs(thing):
+    source_width = lambda t: quack(t, ['width'], ['photo', 'image'])
+    source_height = lambda t: quack(t, ['height'], ['photo', 'image'])
+    return dict(width=source_width(thing), height=source_height(thing))
+
 
 def _get_url_img_obj_from_thing(thing):
     img_object = None
