9190207d89c4b243c833fda9bc0e31833f22ac7c
Aaron McCall <aaron@andyet.net>
2012-11-01 12:59:09 -0700

    [JANUS-457] refactor for testability and deduplication

--- lazythumbs/urls.py
+++ lazythumbs/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls.defaults import patterns
+from django.conf.urls.defaults import patterns, url
 from lazythumbs.views import LazyThumbRenderer
 
 urlpatterns = patterns('',
     # we'll cleanse the liberal .+ in the view.
-    (r'lt_cache/(\w+)/(\d+/\d+|\d+)/(.+)$', LazyThumbRenderer.as_view()),
-    (r'lt_cache/(\w+)/(\d+x\d+|\d+)/(.+)$', LazyThumbRenderer.as_view()),
+    url(r'lt_cache/(\w+)/(\d+/\d+|\d+)/(.+)$', LazyThumbRenderer.as_view(), name='lt_slash_sep'),
+    url(r'lt_cache/(\w+)/(\d+x\d+)/(.+)$', LazyThumbRenderer.as_view(), name='lt_x_sep'),
+    url(r'lt_cache/(\w+)/(x/\d+)/(.+)$', LazyThumbRenderer.as_view(), name='lt_x_width'),
 )
