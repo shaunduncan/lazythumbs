a33d754f06e9e90030bd0a8c38d0c788d0c92f83
Shaun Duncan <shaun.duncan@gmail.com>
2019-02-17 13:58:16 -0500

    Update urlconf

--- lazythumbs/urls.py
+++ lazythumbs/urls.py
@@ -1,13 +1,10 @@
-try:
-    from django.conf.urls import patterns, url
-except ImportError:
-    from django.conf.urls.defaults import patterns, url  # support Django 1.3
+from django.conf.urls import url
 
 from lazythumbs.views import LazyThumbRenderer
 
-urlpatterns = patterns('',
+urlpatterns = [
     # we'll cleanse the liberal .+ in the view.
     url(r'lt_cache/(\w+)/(\d+/\d+|\d+)/(.+)$', LazyThumbRenderer.as_view(), name='lt_slash_sep'),
     url(r'lt_cache/(\w+)/(\d+x\d+)/(.+)$', LazyThumbRenderer.as_view(), name='lt_x_sep'),
     url(r'lt_cache/(\w+)/(x/\d+)/(.+)$', LazyThumbRenderer.as_view(), name='lt_x_width'),
-)
+]
